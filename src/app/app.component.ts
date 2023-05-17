6a63f95122324ce3714f6d98382dd4f28d0ee71c
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2023-05-17 22:28:42 +0200

    feature: add main task behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,22 @@ interface ProgressBarData {
   percentage: number;
 }
 
+class Candidate {
+  name: string;
+  lastName: string;
+
+  constructor(name: string, lastName: string) {
+    this.name = name;
+    this.lastName = lastName;
+  }
+}
+
+const MY_TIMESTAMP = 1684352816703;
+const CANDIDATE = {
+  name: 'Gonzalo',
+  lastName: 'Diaz Ailan',
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,8 +31,28 @@ export class AppComponent implements OnInit {
   title = 'web-atrio-test';
 
   progressData!: ProgressBarData[];
+  testDate: number = MY_TIMESTAMP;
+  candidate: Candidate = new Candidate(CANDIDATE.name, CANDIDATE.lastName);
 
   ngOnInit(): void {
     this.progressData = mockupProgressData;
+  }
+
+  resetControls(): void {
+    this.progressData.forEach((progress) => {
+      progress.percentage = 0;
+    });
+  }
+
+  addToControls(ammount: number): void {
+    const decimalAmmount = ammount / 100;
+    this.progressData.forEach((progress) => {
+      const isOvercharged = progress.percentage + decimalAmmount > 1;
+      if (isOvercharged) {
+        progress.percentage = 1;
+      } else {
+        progress.percentage += decimalAmmount;
+      }
+    });
   }
 }
